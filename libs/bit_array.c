a042b43c36685fa8b99a739b66c7a9105853a409
esuom1 <yerdna.voleyrog@gmail.com>
2020-10-15 12:33:39 -0700

    Added add_byte and check_byte functions + other minor improvements

--- libs/bit_array.c
+++ libs/bit_array.c
@@ -4,40 +4,50 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "bitarray.h"
 
-static uint8_t ONE_BIT = 1;
+#define MASK_00000001 0x01
+#define MASK_00000010 0x02
+#define MASK_00000100 0x04
+#define MASK_00001000 0x08
+#define MASK_00010000 0x10
+#define MASK_00100000 0x20
+#define MASK_01000000 0x40
+#define MASK_10000000 0x80
 
-struct bit_array * bit_array_create(void) {
-    struct bit_array * this = malloc(sizeof(struct bit_array));
+#define BYTE 8
+#define BIT 1
+
+bit_array bit_array_create(void) {
+    bit_array this = calloc(1, sizeof(struct bit_array_s));
     return this;
 }
 
-void bit_array_destroy(struct bit_array ** p_this) {
+void bit_array_destroy(bit_array * p_this) {
+    free((*p_this)->array);
     free(*p_this);
     *p_this = NULL;
 }
 
-void bit_array_init(struct bit_array * this, size_t initial_bit_capacity) {
-    size_t byte_capacity = (initial_bit_capacity + 7) / 8;
+void bit_array_init(bit_array this, size_t initial_bit_capacity) {
+    size_t byte_capacity = (initial_bit_capacity + 7) / BYTE;
     this->array = calloc(byte_capacity, sizeof(uint8_t));
-    this->bit_capacity = byte_capacity * 8;
+    this->bit_capacity = byte_capacity * BYTE;
     this->bit_length = 0;
 }
 
-static void bit_array_set_one(struct bit_array * this, size_t index) {
-    this->array[index / 8] |= (ONE_BIT << (index % 8));
+static void bit_array_set_one(bit_array this, size_t index) {
+    this->array[index / BYTE] |= (BIT << (index % BYTE));
 }
 
-static void bit_array_set_zero(struct bit_array * this, size_t index) {
-    this->array[index / 8] &= ~(ONE_BIT << (index % 8));
+static void bit_array_set_zero(bit_array this, size_t index) {
+    this->array[index / BYTE] &= ~(BIT << (index % BYTE));
 }
 
-static bool bit_array_check(struct bit_array * this, size_t index) {
-    return this->array[(index / 8)] & (ONE_BIT << (index % 8));
+static bool bit_array_check(bit_array this, size_t index) {
+    return this->array[(index / BYTE)] & (BIT << (index % 8));
 }
 
-bool bit_array_check_bit(struct bit_array * this, size_t index) {
+bool bit_array_check_bit(bit_array this, size_t index) {
     if (index >= this->bit_length) {
         fprintf(stderr, "bit_array_set_bit: index out of bounds.\n");
         return -1;
@@ -45,9 +55,27 @@ bool bit_array_check_bit(struct bit_array * this, size_t index) {
     return bit_array_check(this, index);
 }
 
-void bit_array_add_bit(struct bit_array * this, bool bit_value) {
+uint8_t bit_array_check_byte(bit_array this, size_t index) {
+    if ((index + BYTE) > this->bit_length) {
+        fprintf(stderr, "bit_array_set_bit: index out of bounds.\n");
+        return -1;
+    }
+    uint8_t byte = 0;
+    byte += bit_array_check_bit(this, index++) * 128;
+    byte += bit_array_check_bit(this, index++) * 64;
+    byte += bit_array_check_bit(this, index++) * 32;
+    byte += bit_array_check_bit(this, index++) * 16;
+    byte += bit_array_check_bit(this, index++) * 8;
+    byte += bit_array_check_bit(this, index++) * 4;
+    byte += bit_array_check_bit(this, index++) * 2;
+    byte += bit_array_check_bit(this, index++) * 1;
+
+    return byte;
+}
+
+void bit_array_add_bit(bit_array this, bool bit_value) {
     if (this->bit_length >= this->bit_capacity) {
-        fprintf(stderr, "bit_array_add_bit: BitArray capacity reached.\n");
+        fprintf(stderr, "bit_array_add_bit: byte_array capacity reached.\n");
         return;
     }
 
@@ -58,7 +86,7 @@ void bit_array_add_bit(struct bit_array * this, bool bit_value) {
     }
 }
 
-void bit_array_set_bit(struct bit_array * this, size_t index, bool bit_value) {
+void bit_array_set_bit(bit_array this, size_t index, bool bit_value) {
     if (index >= this->bit_length) {
         fprintf(stderr, "bit_array_set_bit: index out of bounds.\n");
         return;
@@ -71,7 +99,33 @@ void bit_array_set_bit(struct bit_array * this, size_t index, bool bit_value) {
     }
 }
 
-void bit_array_pop_bit(struct bit_array * this) {
+void bit_array_pop_bit(bit_array this) {
     if (this->bit_length == 0) return;
     bit_array_set_zero(this, --this->bit_length);
+}
+
+static bool get_bit_value(uint8_t val, uint8_t mask) {
+    uint8_t masked = val & mask;
+
+    if(masked > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void bit_array_add_byte(bit_array this, uint8_t byte_value) {
+    if ((this->bit_length + BYTE) > this->bit_capacity) {
+        fprintf(stderr, "bit_array_add_byte: byte_array capacity reached.\n");
+        return;
+    }
+
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_10000000));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_01000000));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00100000));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00010000));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00001000));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00000100));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00000010));
+    bit_array_add_bit(this, get_bit_value(byte_value, MASK_00000001));
 }
